dea60ab836eaf4fde88069bdceced2be71125caf
Artem <artemtricks@yandex.ru>
2023-02-20 11:57:15 +0700

    home page

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,15 @@
 import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import Header from './Components/header';
+import Home from './Pages/Home';
 
 
 function App() {
   return (
     <div>
       <Header />
+      <Home />
+
     </div>
   );
 }
